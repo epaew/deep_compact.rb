47142acf7fe1b47654e282afceee1261d535fc14
epaew <epaew.333@gmail.com>
2021-04-10 08:24:40 +0900

    Run `bundle gem deep_compact`

---
+++ lib/deep_compact.rb
@@ -0,0 +1,6 @@
+require "deep_compact/version"
+
+module DeepCompact
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/deep_compact/version.rb
@@ -0,0 +1,3 @@
+module DeepCompact
+  VERSION = "0.1.0"
+end
