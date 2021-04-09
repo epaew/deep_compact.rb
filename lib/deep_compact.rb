12dd38b2f99b5f5bcafaeb497508de17b74f0010
epaew <epaew.333@gmail.com>
2021-04-10 07:20:50 +0900

    Add base of DeepCompact module

--- lib/deep_compact.rb
+++ lib/deep_compact.rb
@@ -1,8 +1,21 @@
 # frozen_string_literal: true
 
-require 'deep_compact/version'
+require_relative 'deep_compact/version'
 
 module DeepCompact
-  class Error < StandardError; end
-  # Your code goes here...
+  refine ::Array do
+    def deep_compact
+      compact.map do |value|
+        value.respond_to?(:deep_compact) ? value.deep_compact : value
+      end
+    end
+  end
+
+  refine ::Hash do
+    def deep_compact
+      compact.transform_values do |value|
+        value.respond_to?(:deep_compact) ? value.deep_compact : value
+      end
+    end
+  end
 end
