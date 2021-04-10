72f6fb8618aedb82aaef01b042eb853dcf525489
epaew <epaew.333@gmail.com>
2021-04-10 11:29:47 +0900

    Update workflows/rspec
    - Send coverage result to codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,10 +2,17 @@
 
 require 'simplecov'
 require 'simplecov-console'
+require 'simplecov_json_formatter'
 
 SimpleCov.start do
+  add_filter '/spec/'
   enable_coverage :branch
-  formatter SimpleCov::Formatter::Console
+  formatter SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::JSONFormatter, # for codeclimate
+      SimpleCov::Formatter::Console        # for stdout
+    ]
+  )
 end
 
 require 'bundler/setup'
